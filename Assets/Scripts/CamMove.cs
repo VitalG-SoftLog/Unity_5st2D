37ed1b3033411f31185f1362b3ce201ade1899cd
Vitaliy Gusev <vitalg@sol-softlogic.com>
2018-01-24 22:57:35 +0500

    Initial commit

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CamMove : MonoBehaviour {
+
+	public GameObject GG5;
+
+	void Update () {
+		transform.position = new Vector3 (GG5.transform.position.x, GG5.transform.position.y, -7f);
+		
+	}
+}
---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+	public float speed = 7f;
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent <Rigidbody2D> ();
+	}
+
+	void Update () {
+		float moveX = Input.GetAxis ("Horizontal");
+		rb.MovePosition (rb.position + Vector2.right * moveX);
+
+		if (Input.GetKeyDown (KeyCode.Space))
+			rb.AddForce (Vector2.up * 5000);
+
+	}
+}
